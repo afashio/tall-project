7b57817cbe0d3dc5f000b92bf37347a008020ba3
afashio <push.webstudio@gmail.com>
2021-04-26 21:52:42 +0300

    Create: modal component

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,1 +1,11 @@
-<?php
+@props(['trigger'])
+<div
+    class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
+    x-show="{{ $trigger }}"
+    @click.self="{{ $trigger }} = false"
+    @keydown.escape.window="{{ $trigger }} = false"
+>
+    <div {{ $attributes->merge(['class' => 'm-auto bg-gray-500 shadow-2xl rounded-xl p-8']) }} >
+       {{ $slot }}
+    </div>
+</div>
